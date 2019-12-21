9be8aaa82497b0d6aabb1ad79512fb3df0726c65
CapArvin <cap_arvin@126.com>
2019-12-21 13:50:10 +0800

    zhifu xiugai

--- common/component/Payment/WxPay/WxPayConf_pub.php
+++ common/component/Payment/WxPay/WxPayConf_pub.php
@@ -9,9 +9,10 @@ class WxPayConf_pub
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
 	const APPID = 'wx538bb8de1b0f1074';
 	//受理商ID，身份标识
-	const MCHID = '1236247902';
+	const MCHID = '1567541271';
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	const KEY = 'A6D73B16CBC9A7F940B5A523F7985AE5';
+//	const KEY = 'A6D73B16CBC9A7F940B5A523F7985AE5';
+    const KEY = '184a7eb6e4df4a20ce97e9fd345ac322';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
 	const APPSECRET = 'dcedb30a7ec916a09a9fc5e82fbbd6f7';
 	
@@ -21,9 +22,10 @@ class WxPayConf_pub
 	
 	//=======【证书路径设置】=====================================
 	//证书路径,注意应该填写绝对路径
-	const SSLCERT_PATH = '/cacert/apiclient_cert.pem';
-	const SSLKEY_PATH = '/cacert/apiclient_key.pem';
-	
+//	const SSLCERT_PATH = '/cacert/apiclient_cert.pem';
+//	const SSLKEY_PATH = '/cacert/apiclient_key.pem';
+    const SSLCERT_PATH = '';
+    const SSLKEY_PATH ='';
 	//=======【异步通知url设置】===================================
 	//异步通知url，商户根据实际开发过程设定
 	const NOTIFY_URL = 'https://open.mrhuigou.com/payment/weixin';
