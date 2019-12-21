c113cb236e1850f8ced805302412161a7b2e5d37
CapArvin <cap_arvin@126.com>
2019-12-21 14:21:13 +0800

    jihuarenwu

---
+++ console/config/main-local.php
@@ -0,0 +1,55 @@
+<?php
+return [
+	'controllerMap' => [
+		'cron' => [
+			'class' => 'console\controllers\CronController',
+			'cronJobs' =>[
+//				'old/data/index' => [
+//					'cron'      => '* * * * *',
+//				],
+//				'old/data/init' => [
+//					'cron'      => '0 1 */1 * *',
+//				],
+				'old/open/index' => [
+					'cron'      => '0 1 */1 * *',
+				],
+				'old/open/update' => [
+					'cron'      => '* * * * *',
+				],
+				'old/order/auto' => [
+					'cron'      => '* * * * *',
+				],
+				'old/express/order' => [
+					'cron'      => '* * * * *',
+				],
+				'old/express/status' => [
+					'cron'      => '*/2 * * * *',
+				],
+				'old/order/delivery-comment' => [
+					'cron'      => '*/3 * * * *',
+				],
+				'old/order/status' => [
+					'cron'      => '*/2 * * * *',
+				],
+				'old/order/cancel' => [
+					'cron'      => '*/3 * * * *',
+				],
+				'old/stock/auto' => [
+					'cron'      => '*/3 * * * *',
+				],
+				'old/stock/autoschedule' => [
+					'cron'      => '*/3 * * * *',
+				],
+//                'old/notice/coupon' => [
+//                    'cron'      => '0,30 09-17 * * *',
+//                ],
+                'old/order/auto-receive' => [
+                    'cron'      => '*/3 * * * *',
+                ],
+//                'old/notice/srwd-point' => [
+//                    'cron'      => ' 00 00 01 02 *',
+//                ],
+			],
+		],
+	],
+];
