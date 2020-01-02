79e1ccde5d5561023793e492ad3ae64ae5d84337
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-01-02 10:30:32 +0800

    抽奖的页面的分享

--- h5/views/choujiang/index-new.php
+++ h5/views/choujiang/index-new.php
@@ -257,8 +257,8 @@ $this->registerJs($this->blocks['J_Reviews'],\yii\web\View::POS_END);
 <?php h5\widgets\Tools\Share::widget(['data'=>[
 	'title' => '同城网购，尽在每日惠购！',
 	'desc' => '酸奶、水果、饮料、日常用品。来每日惠购就“购”了!',
-	'link' => \yii\helpers\Url::to(['/site/index'],true),
+	'link' => \yii\helpers\Url::to(['/choujiang/index-new'],true),
 	'image' => Yii::$app->request->getHostInfo() . '/assets/images/hongbao_share.jpg',
-    'hidden_status'=>'hidden',
+//    'hidden_status'=>'hidden',
 ]]);
 ?>
