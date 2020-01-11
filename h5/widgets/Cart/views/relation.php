b306669809b44cec66ee9132f4ac2059d4fa617e
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-01-11 15:23:43 +0800

    购物车 页面跳转首页按钮样式修改

--- h5/widgets/Cart/views/relation.php
+++ h5/widgets/Cart/views/relation.php
@@ -7,10 +7,8 @@
  */
 ?>
 <!--<div class="tc f16  w green p5 lh200 pt10"> // // 买 了 又 看 // // </div>-->
-<div class="tc f16  w green p5 lh200 pt10">
-    <a class="green" href="https://m.mrhuigou.com/site/index">
-        // // 点击继续购物 // //
-    </a>
+<div class="p5">
+    <a class="btn mbtn greenbtn-bd tc w" href="/site/index">点击继续购物</a>
 </div>
 
 <dl class="mt10">
