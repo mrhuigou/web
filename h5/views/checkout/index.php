8fabf589211b45386362da23381232adaf6e99f9
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-01-15 16:00:18 +0800

    订单确认页面

--- h5/views/checkout/index.php
+++ h5/views/checkout/index.php
@@ -106,9 +106,9 @@ $this->title = '订单确认';
                 </script>
                     <div class="p5">
                         <?php if( count($checkout_ad) < 1){?>
-                            <a class="btn mbtn greenbtn-bd tc w" href="/read-more/index">满68包邮，去凑单</a>
+                            <a class="btn mbtn greenbtn-bd tc w" href="/read-more/index">满68包邮，去凑单11</a>
                         <?php }else{?>
-                            <a class="btn mbtn greenbtn-bd tc w layerTri" href="javascript:void(0)">满68包邮，去凑单<?php '总'.$total.'定'.$sub_total?></a>
+                            <a class="btn mbtn greenbtn-bd tc w layerTri" href="javascript:void(0)">满68包邮，去凑单1<?= '总'.$total.'定'.$sub_total?></a>
                         <?php }?>
 
                     </div>
