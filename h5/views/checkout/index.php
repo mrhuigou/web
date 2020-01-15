e2afd7fed69530e1a93930c43d918199e0603b11
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-01-15 17:48:07 +0800

    地宫

--- h5/views/checkout/index.php
+++ h5/views/checkout/index.php
@@ -175,51 +175,19 @@ $this->title = '订单确认';
                 <?php } ?>
 
 
-<!--                <div class="graybg p10 store_totals">-->
-<!--					--><?php //if ($val['totals']) { ?>
-<!--						--><?php //foreach ($val['totals'] as $value) { ?>
-<!--                            <p class="mb5 clearfix lh150 total_line" id = "--><?php //if($value['code'] =='points'){ echo 'point_customer_total_'.$value['customer_code_id'];}?><!--" >-->
-<!--                                <span class="fr red fb">￥<em-->
-<!--                                            class="--><?//= $value['code'] ?><!--">--><?//= $value['value'] ?><!--</em></span>-->
-<!--                                <span class="fl fb">--><?//= $value['title'] ?><!--：</span>-->
-<!--                            </p>-->
-<!--						--><?php //} ?>
-<!--					--><?php //} ?>
-<!---->
-<!--                </div>-->
-
                 <div class="graybg p10 store_totals">
-                    <?php if ($val['totals']) { ?>
-                        <?php foreach ($val['totals'] as $value) { ?>
-                            <?php
-                            if($value['code'] == 'total' || $value['code'] == 'shipping') {
-                                continue;
-                            }
-                            ?>
+					<?php if ($val['totals']) { ?>
+						<?php foreach ($val['totals'] as $value) { ?>
                             <p class="mb5 clearfix lh150 total_line" id = "<?php if($value['code'] =='points'){ echo 'point_customer_total_'.$value['customer_code_id'];}?>" >
                                 <span class="fr red fb">￥<em
                                             class="<?= $value['code'] ?>"><?= $value['value'] ?></em></span>
                                 <span class="fl fb"><?= $value['title'] ?>：</span>
                             </p>
-                        <?php } ?>
-                    <?php } ?>
+						<?php } ?>
+					<?php } ?>
 
                 </div>
 
-                <div class="graybg p10 store_totals">
-                    <?php if ($val['totals']) { ?>
-                        <?php foreach ($val['totals'] as $value) { ?>
-                            <?php if($value['code'] == 'total' || $value['code'] == 'shipping') {?>
-                                <p class="mb5 clearfix lh150 total_line" id = "<?php if($value['code'] =='points'){ echo 'point_customer_total_'.$value['customer_code_id'];}?>" >
-                                    <span class="fr red fb">￥<em
-                                                class="<?= $value['code'] ?>"><?= $value['value'] ?></em></span>
-                                    <span class="fl fb"><?= $value['title'] ?>：</span>
-                                </p>
-                            <?php } ?>
-                        <?php } ?>
-                    <?php } ?>
-
-                </div>
             </div>
             <div class="mt5 mb50">
                 <input type="text" class="w p10 gray6 bd" name="CheckoutForm[comment][<?= $k ?>]" placeholder="备注：如有特殊需求请填写" maxlength="50">
