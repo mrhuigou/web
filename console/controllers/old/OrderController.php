982a05d0aa06251acb4f80f43ee1ec51695e2ffa
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-01-18 16:23:05 +0800

    未支付订单微信通知

--- console/controllers/old/OrderController.php
+++ console/controllers/old/OrderController.php
@@ -800,7 +800,7 @@ class OrderController extends \yii\console\Controller {
                         if ($open_id = $model->customer->getWxOpenId()) {
                             $message = "";
                             $notice = new WxNotice();
-                            $notice->order($open_id, "http://m.mrhuigou.com/order/index", ['title' => '尊敬的每日惠购会员', 'order_no' => $model->order_no, 'total' => $model->total, 'status' => '订单未支付，请及时支付！', 'remark' => $message]);
+                            $notice->order($open_id, "http://m.mrhuigou.com/order/index", ['title' => '尊敬的每日惠购会员', 'order_no' => $model->order_no, 'total' => $model->total, 'status' => '你有未支付订单,将8分钟之后自动取消,如需购买请及时支付！', 'remark' => $message]);
                         }
                     }
                 }
