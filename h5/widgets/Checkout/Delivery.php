f263f0ca00a116fae559252cc4229bde67d6d051
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-02-15 15:48:49 +0800

    明天上午配送关闭

--- h5/widgets/Checkout/Delivery.php
+++ h5/widgets/Checkout/Delivery.php
@@ -23,10 +23,10 @@ class Delivery extends Widget
             throw new InvalidParamException("参数错误");
         }
         $this->exclude=[
-            'start_time'=>strtotime('2020-02-15'),
-	        'start_index'=>1, //配送时间段，0为早间 1为午间，2为午间 3为晚间
-            'end_time'=>strtotime('2020-02-15'),
-	        'end_index'=>2
+            'start_time'=>strtotime('2020-02-16'),
+	        'start_index'=>0, //配送时间段，0为早间 1为午间，2为午间 3为晚间
+            'end_time'=>strtotime('2020-02-16'),
+	        'end_index'=>0
         ];
         $method_times=$this->getList($store);
         if(($order_delivery_dates=\Yii::$app->request->post('CheckoutForm')) && isset($order_delivery_dates['delivery'][$store->store_id]['date']) && $order_delivery_dates['delivery'][$store->store_id]['date']){
