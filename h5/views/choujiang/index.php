1d1c8c3f2f292c82132753eee6c541761bb15ff3
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-02-25 13:50:49 +0800

    抽奖页面打开分享功能

--- h5/views/choujiang/index.php
+++ h5/views/choujiang/index.php
@@ -246,6 +246,6 @@ $this->registerJs($this->blocks['J_Reviews'],\yii\web\View::POS_END);
 	'desc' => '酸奶、水果、饮料、日常用品。来每日惠购就“购”了!',
 	'link' => \yii\helpers\Url::to(['/site/index'],true),
 	'image' => Yii::$app->request->getHostInfo() . '/assets/images/hongbao_share.jpg',
-    'hidden_status'=>'hidden',
+//    'hidden_status'=>'hidden',
 ]]);
 ?>
