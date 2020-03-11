6814991708543b82659fbc6085d1682ca52b71d9
chenhao668 <qc_73d80c9cdd9c45e8b7d5dab09563fe94@git.code.tencent.com>
2020-03-11 18:13:26 +0800

    fx  订单支付

---
+++ affiliate/views/payment/error.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+$this->title = "支付错误";
+?>
+<?=h5\widgets\Header::widget(['title'=>$this->title])?>
+<section class="veiwport  mb50">
+    <figure class="info-tips red">
+        <i class="iconfont font-warning ">&#xe616;</i>
+        <figcaption class="mt10"> <?= nl2br(Html::encode($message)) ?></figcaption>
+    </figure>
+    <div class="white">
+    <a class="btn mbtn db mt10 orgbg w br5"  href="javascript:history.back();">返回</a>
+    </div>
+</section>
+<?= h5\widgets\MainMenu::widget(); ?>
