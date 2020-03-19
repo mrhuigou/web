8eeb7fa6378570880ecc6bccfcc679f7b6f58815
BroHao999 <13287379532@163.com>
2020-03-19 14:50:50 +0800

    优惠券到期时间更新 跟着更新已经领取 用户券的到期时间

--- api/controllers/WebapiController.php
+++ api/controllers/WebapiController.php
@@ -28,6 +28,7 @@ use api\models\V1\CouponCate;
 use api\models\V1\CouponCateToCoupon;
 use api\models\V1\CouponGift;
 use api\models\V1\CouponProduct;
+use api\models\V1\CustomerCoupon;
 use api\models\V1\District;
 use api\models\V1\GroundPushPlan;
 use api\models\V1\GroundPushPlanView;
@@ -1296,7 +1297,20 @@ class WebapiController extends \yii\rest\Controller {
 			foreach ($datas as $data) {
 				if (!$model = Coupon::findOne(['code' => trim($data['CODE'])])) {
 					$model = new Coupon();
-				}
+				}else{
+				    //如果更新到期时间，更新所有已经领取过的用户券的到期时间
+                    if(!empty($data['END_TIME'])){
+                        if($model->date_end != $data['END_TIME']){
+                            $customer_coupons = CustomerCoupon::find()->where(['is_use' => 0, 'coupon_id' => $model->coupon_id])->all();
+                            foreach ($customer_coupons as $customer_coupon){
+                                $customer_coupon->end_time = $data['END_TIME'];
+                                if (!$customer_coupon->save()) {
+                                    throw new \Exception(serialize($customer_coupon->errors));
+                                }
+                            }
+                        }
+                    }
+                }
 				$model->code = trim($data['CODE']);
 				$model->name = $data['NAME'];
 				$model->comment = $data['DESCRIPTION'];
