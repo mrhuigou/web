d934f64b9e392ca1bd9e9bdbeb323cc818b95b12
BroHao999 <13287379532@163.com>
2020-03-26 11:51:57 +0800

    fx 团购分销页面去掉表头

--- affiliate/views/affiliate-plan/index.php
+++ affiliate/views/affiliate-plan/index.php
@@ -9,19 +9,19 @@ use yii\helpers\Url;
 $this->title = "一起团";
 ?>
 <header class="fx-top bs-bottom whitebg lh44">
-    <div class="flex-col tc">
-        <a class="flex-item-2" href="#">
-            <i class="aui-icon aui-icon-home white f28"></i>
-        </a>
-        <div class="flex-item-8 f16">
-			<?= \yii\helpers\Html::encode($this->title) ?>
-        </div>
-        <a class="flex-item-2" href="<?= \yii\helpers\Url::to(['/user/index']) ?>">
-            <i class="iconfont green f28">&#xe603;</i>
-        </a>
-    </div>
+<!--    <div class="flex-col tc">-->
+<!--        <a class="flex-item-2" href="#">-->
+<!--            <i class="aui-icon aui-icon-home white f28"></i>-->
+<!--        </a>-->
+<!--        <div class="flex-item-8 f16">-->
+<!--			--><?//= \yii\helpers\Html::encode($this->title) ?>
+<!--        </div>-->
+<!--        <a class="flex-item-2" href="--><?//= \yii\helpers\Url::to(['/user/index']) ?><!--">-->
+<!--            <i class="iconfont green f28">&#xe603;</i>-->
+<!--        </a>-->
+<!--    </div>-->
 </header>
-<div class="content  bc">
+<div class="content-new  bc" >
     <!-- 新改图片 -->
 <!--    <div class="bg-wh pb5 mb5">-->
 <!--        <a href="#">-->
