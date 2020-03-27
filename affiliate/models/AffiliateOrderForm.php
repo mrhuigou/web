8e11f889270af2324a7527fc6b1cf45a38ba57d3
BroHao999 <13287379532@163.com>
2020-03-27 14:01:43 +0800

    分销获取默认的 配送方式修改

--- affiliate/models/AffiliateOrderForm.php
+++ affiliate/models/AffiliateOrderForm.php
@@ -15,38 +15,6 @@ class AffiliateOrderForm extends Affiliate {
     public $address = [];
     public function __construct($affiliate_id=0,$fx_user_info)
     {
-        //获取自己默认的收货地址
-        if (\Yii::$app->session->get('checkout_address_id')) {
-            $this->address_id = \Yii::$app->session->get('checkout_address_id');
-        }else{
-
-        }
-
-        $affiliate_info = Affiliate::find()->where(['status'=>1,'affiliate_id'=>$affiliate_id])->one();
-        if($affiliate_info->mode == 'DOWN_LINE'){//线下
-            $this->distribution_type = 2;
-        }
-        if($this->distribution_type == 2){ //获取团长的配送地址
-//            $address['zone_id'] = $affiliate_info->zone_id;
-//            $address['city_id'] = $affiliate_info->city_id;
-//            $address['district_id'] = $affiliate_info->district_id;
-            $this->address['zone'] = $affiliate_info->zone_name;
-            $this->address['city'] = $affiliate_info->city_name;
-            $this->address['district'] = $affiliate_info->district_name;
-            $this->address['address_1'] = $affiliate_info->address;
-            $this->address['address_username'] = $fx_user_info['firstname']?:"hdogh";
-            $this->address['address_telephone'] = $fx_user_info['telephone'];
-        }
-        //获取最后下单的地址
-        $last_order_info = Order::find()->where(['customer_id'=> $fx_user_info['customer_id'],'sent_to_erp'=> 'Y'])->orderBy('date_added desc')->one();
-        if($last_order_info){
-            $this->address['zone'] = $last_order_info->orderShipping->shipping_zone;
-            $this->address['city'] = $last_order_info->orderShipping->shipping_city;
-            $this->address['district'] = $last_order_info->orderShipping->shipping_district;
-            $this->address['address_1'] = $last_order_info->orderShipping->shipping_address_1;
-            $this->address['address_username'] = $last_order_info->orderShipping->shipping_firstname;
-            $this->address['address_telephone'] = $last_order_info->orderShipping->shipping_telephone;
-        }
         parent::__construct();
     }
 
