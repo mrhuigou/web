50f0bd0f45ea0298163a77f4fea2b6110dad9514
BroHao999 <13287379532@163.com>
2020-03-29 03:50:18 +0800

    分销管理端 回复原来

--- affiliate/views/user/balance-list-item.php
+++
@@ -1,7 +0,0 @@
-<div class="bdb whitebg p10 clearfix">
-	<div class="fl pw80">
-		<p class="f14 mxh14 mb5"><?= $model->description; ?></p>
-		<span class="gray9"><?= $model->date_added; ?></span>
-	</div>
-	<div class="fr pw20 gray6 f16 tr mt10 <?= $model->amount < 0 ? 'red' : ''; ?>">￥<?= $model->amount; ?></div>
-</div>
