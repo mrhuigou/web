f015b457334bc41a02a503f0011edcc9bc290940
BroHao999 <13287379532@163.com>
2020-03-29 02:29:51 +0800

    删除多余的文件 fx 模块

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,8 +2,8 @@
 Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('h5', dirname(dirname(__DIR__)) . '/h5');
-Yii::setAlias('fx', dirname(dirname(__DIR__)) . '/fx');
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('affiliate', dirname(dirname(__DIR__)) . '/affiliate');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('fx', dirname(dirname(__DIR__)) . '/fx');
