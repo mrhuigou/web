ac329ba1d4ed8c6f5f87d123f960155f604b53cc
BroHao999 <13287379532@163.com>
2020-05-11 11:17:19 +0800

    fx 导入 page 页面

---
+++ fx/controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+namespace fx\controllers;
+
+use api\models\V1\Page;
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+
+/**
+ * Site controller
+ */
+class PageController extends Controller
+{
+    public function actionIndex(){
+			if (\Yii::$app->user->isGuest) {
+	return $this->redirect(['/site/login','redirect'=>\Yii::$app->request->getAbsoluteUrl()]);
+	}
+
+        $page_id = Yii::$app->request->get("page_id");
+        $page = Page::find()->where(['page_id'=>$page_id])->one();
+        if(!empty($page)){
+//            $referer = Yii::$app->request->referrer;
+//            if(strpos($referer,'weixinbridge')){
+//                //Yii::$app->request->absoluteUrl;
+//                $_SERVER['HTTP_REFERER'] = 'https://m.mrhuigou.com/';
+//                $this->redirect('https://m.mrhuigou.com/page/'.$page_id.'.html');
+//            }
+            return $this->render("index",['page'=>$page]);
+        }else{
+            throw new NotFoundHttpException("没有找到该页面！");
+        }
+    }
+}
