5b3ef85fced61b95ebe917eff0788daad6a51ce2
BroHao999 <13287379532@163.com>
2020-07-02 08:39:54 +0800

    分销管理端 logo 处理

--- affiliate/widgets/views/Header.php
+++ affiliate/widgets/views/Header.php
@@ -3,9 +3,19 @@
     <!-- BEGIN TOP NAVIGATION BAR -->
     <div class="header-inner">
         <!-- BEGIN LOGO -->
-        <a class="navbar-brand" href="<?=Yii::$app->homeUrl?>" style="padding: 5px 0;">
-            <img src="http://img1.mrhuigou.com/group1/M00/06/E6/wKgB7l7sLsiAZnE_AAAbvrBPGTg113.png" alt="logo" class="img-responsive" />
-        </a>
+        <?php  if(Yii::$app->user->getId() == 9){ //地铁?>
+            <a class="navbar-brand" href="<?=Yii::$app->homeUrl?>" style="padding: 5px 0;">
+                <img src="http://img1.mrhuigou.com/group1/M00/06/EB/wKgB7l777LeAJ1eAAAAPXVHpRm8601.jpg" alt="logo" class="img-responsive" />
+            </a>
+        <?php }else{?>
+            <a class="navbar-brand" href="<?=Yii::$app->homeUrl?>" style="padding: 5px 0;">
+                <img src="http://img1.mrhuigou.com/group1/M00/06/E6/wKgB7l7sLsiAZnE_AAAbvrBPGTg113.png" alt="logo" class="img-responsive" />
+            </a>
+        <?php }?>
+
+<!--        <a class="navbar-brand" href="--><?//=Yii::$app->homeUrl?><!--" style="padding: 5px 0;">-->
+<!--            <img src="http://img1.mrhuigou.com/group1/M00/06/E6/wKgB7l7sLsiAZnE_AAAbvrBPGTg113.png" alt="logo" class="img-responsive" />-->
+<!--        </a>-->
         <!-- END LOGO -->
         <!-- BEGIN RESPONSIVE MENU TOGGLER -->
         <a href="javascript:;" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
